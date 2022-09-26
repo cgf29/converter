88e92d2f2acb4d6557ee02cd20e53769ff34894c
Vanya Svyatun <vanyasv06@gmail.com>
2022-09-26 23:23:43 +0300

    first commit

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -8,9 +8,14 @@ const Header = () => {
 
     const getPrice = (currency) => axios.get(`https://v6.exchangerate-api.com/v6/499fe1f958491e801322d0e8/pair/${currency}/UAH`).then(res => res.data.conversion_rate.toFixed(2))
 
-    useEffect(async () => {
-        setUsd(await getPrice('USD'))
-        setEur(await getPrice('EUR'))
+    const fetchAndSetPrice = async (currency) => {
+        const price = await getPrice(currency)
+        currency === 'EUR' ? setEur(price) : setUsd(price)
+    }
+
+    useEffect(() => {
+        fetchAndSetPrice('USD')
+        fetchAndSetPrice('EUR')
     }, [])
 
     return (
